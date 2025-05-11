2cc87d0f2dd62aea57cc9bc89c07f6ab7c7e5ffa
poqob <mmermmus123@gmail.com>
2025-05-11 04:53:49 +0300

    update.

--- zorba/app.py
+++ zorba/app.py
@@ -1,25 +1,29 @@
-from flask import Flask,render_template,jsonify,url_for
+from flask import Flask, render_template, jsonify, send_from_directory
 import os
 import random
 from src.zorba import kufurler
 
-app = Flask(__name__, template_folder='templates', static_folder='content',static_url_path='/zorba/content')  # This tells Flask where templates and static content are located
+# Create Flask application with explicit folder configurations
+app = Flask(__name__, 
+           template_folder='templates')
 
-# Static dosyalara URL ulaşımını düzenleyen özel URL kuralı
-@app.route('/static/<path:filename>')
-def static_files(filename):
-    return app.send_static_file(filename)
+# Main route to serve the HTML page
+@app.route('/zorba')
+def serve_zorba():
+    return render_template('zorba.html')
 
+# Routes to serve static files from the templates directory
+@app.route('/zorba/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('templates', filename)
+
+# API endpoint to serve random "kufur"
 @app.route('/zorba-api')
 def serve_zorba_api():
     # Rastgele bir küfür seç
     secilen_kufur = random.choice(kufurler)
     return jsonify({"kufur": secilen_kufur})
 
-@app.route('/zorba')
-def serve_zorba():
-    return render_template('zorba.html')
-
-
 if __name__ == '__main__':
+    # Using a different port than the default 5000 to avoid conflicts
     app.run(host='127.0.0.1', port=5000)
